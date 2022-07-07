b68fb1aa7c36781b0442c78347512a7f0f00e095
cuitianliang <cuitianliang@hnnewsoft.com>
2022-07-07 16:54:11 +0800

    feat: lib build

---
+++ packages/shared/rollup.config.js
@@ -0,0 +1,20 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: ['src/index.ts'],
+  output: [
+    {
+      dir: 'dist',
+      entryFileNames: '[name].js',
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
+      dir: 'dist',
+      entryFileNames: '[name].js',
+      format: 'es',
+      exports: 'named',
+    },
+  ],
+  plugins: [typescript()],
+};
