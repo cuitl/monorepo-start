9b2d305c9e2f61392501e8c2d2b5ea48c52b2873
cuitianliang <cuitianliang@hnnewsoft.com>
2022-07-08 18:35:20 +0800

    feat: shared named to @vue-start-monorepo/shared

--- apps/vue-h5-start/src/main.ts
+++ apps/vue-h5-start/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { sayHello } from 'shared';
+import { sayHello } from '@vue-start-monorepo/shared';
 
 createApp(App).use(router).mount('#app');
 
--- apps/vue-pc-start/src/main.ts
+++ apps/vue-pc-start/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import { useLog } from '@/hooks';
-import { sayHello } from 'shared';
+import { sayHello } from '@vue-start-monorepo/shared';
 
 useLog();
 
